88c2c994bebdb3d46d6dd06ffb96d91ae5bca4a7
TasfiaIslam <tasf1a027@gmail.com>
2021-07-15 00:24:52 +0600

    Design dashboard

--- frontend/components/Main.js
+++ frontend/components/Main.js
@@ -1,14 +1,66 @@
 const Main = () => {
     return ( 
         <div className="min-h-screen bg-gray-200 rounded-tl-3xl">
-            <div className="p-16">
-                <div className="float-left">
-                    Dashboard
+            <div className="w-11/12 mx-auto">
+                <div className="py-4 flex justify-between border-b border-gray-300">
+                    <div className="text-gray-500 font-semibold text-xl">
+                        Dashboard
+                    </div>
+                    <div className="">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="bg-blue-400 rounded h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                    </div>
                 </div>
-                <div className="bg-blue-200 rounded-full float-right h-6 w-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                    </svg>
+                <div className="my-10 grid grid-cols-4 gap-4 relative">
+                    <div className="h-48 bg-white rounded-lg flex flex-col relative">
+                        <div className="w-3/4 mx-auto mt-6">
+                            <h1 className="text-xl mb-2">Tutors</h1>
+                            <p className="text-sm text-gray-500 mb-2">Total tutors - 400</p>
+                            <p className="text-sm text-gray-500">Lorem Ipsum</p>
+                        </div>
+                        <div className="absolute -top-4 left-6 rounded-full bg-red-300 h-8 w-8 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="h-48 bg-white rounded-lg flex flex-col relative">
+                        <div className="w-3/4 mx-auto mt-6">
+                            <h1 className="text-xl mb-2">Students</h1>
+                            <p className="text-sm text-gray-500 mb-2">Total Students - 400</p>
+                            <p className="text-sm text-gray-500">Lorem Ipsum</p>
+                        </div>
+                        <div className="absolute -top-4 left-6 rounded-full bg-green-300 h-8 w-8 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="h-48 bg-white rounded-lg flex flex-col relative">
+                        <div className="w-3/4 mx-auto mt-6">
+                            <h1 className="text-xl mb-2">Jobs</h1>
+                            <p className="text-sm text-gray-500 mb-2">Total jobs - 400</p>
+                            <p className="text-sm text-gray-500">Lorem Ipsum</p>
+                        </div>
+                        <div className="absolute -top-4 left-6 rounded-full bg-blue-300 h-8 w-8 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="h-48 bg-white rounded-lg flex flex-col relative">
+                        <div className="w-3/4 mx-auto mt-6">
+                            <h1 className="text-xl mb-2">Jobs</h1>
+                            <p className="text-sm text-gray-500 mb-2">Total jobs - 400</p>
+                            <p className="text-sm text-gray-500">Lorem Ipsum</p>
+                        </div>
+                        <div className="absolute -top-4 left-6 rounded-full bg-yellow-300 h-8 w-8 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                            </svg>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
