5413078fcfcca64e2efa32d9113a954365321316
TasfiaIslam <tasf1a027@gmail.com>
2021-07-18 22:10:58 +0600

    Add strapi  backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3763cd78ddc17a351ff5d148ae696298'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1f9916c3-1278-4120-8bf2-5b54746412f5'
+};
