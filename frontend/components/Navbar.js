3306c8fc51257f4037e59b764630fe35d3e1fa1b
TasfiaIslam <tasf1a027@gmail.com>
2021-07-18 06:03:43 +0600

    Modify navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,20 +1,18 @@
 const Navbar = () => {
     return ( 
-        <div className="h-12">
-          <ul className="flex">
-            <li className="mr-6">
-              <a className="text-blue-500 hover:text-blue-800" href="#">Active</a>
-            </li>
-            <li className="mr-6">
-              <a className="text-blue-500 hover:text-blue-800" href="#">Link</a>
-            </li>
-            <li className="mr-6">
-              <a className="text-blue-500 hover:text-blue-800" href="#">Link</a>
-            </li>
-            <li className="mr-6">
-              <a className="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
-            </li>
-          </ul>     
+        <div className="">
+          <div className="flex justify-between mx-12 my-4">
+                <div className="flex justify-between space-x-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  </svg>
+                  <input type="text" placeholder="Search.."></input>
+                </div>
+                <div className="space-x-4">
+                  <a className="text-gray-500 hover:text-blue-500" href="#">Notifications</a>
+                  <a className="text-gray-500 hover:text-blue-500" href="#">Profile</a>
+                </div>
+          </div>
         </div>
      );
 }
